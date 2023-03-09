97355a96746486af24822edd32067acf28810bb0
Katipelli Rakesh <rakesh.katipelli@volvocars.com>
2023-03-09 18:19:04 +0530

    mod 4

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using StarChart.Data;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace StarChart.Controllers
+{
+    [Route(""), ApiController]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+    }
+}
